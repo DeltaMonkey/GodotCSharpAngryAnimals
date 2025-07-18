1b5c15bfaaeb1cd399123126e86805fe5f18d454
DeltaMonkey <engincanozkan@outlook.com>
2025-07-18 15:22:29 +0300

    Animal Scene Setup Challange

--- Scenes/Animal/Animal.cs
+++ Scenes/Animal/Animal.cs
@@ -3,26 +3,61 @@ using System;
 
 public partial class Animal : RigidBody2D
 {
-	[Export] private Label _label;
+	public enum AnimalState { READY, DRAG, RELEASE }
+
+	[Export] private Label _debugLabel;
 	[Export] private AudioStreamPlayer2D _stretchSound;
 	[Export] private AudioStreamPlayer2D _launchSound;
 	[Export] private AudioStreamPlayer2D _kickSound;
 	[Export] private Sprite2D _arrow;
 	[Export] private VisibleOnScreenNotifier2D _visibleOnScreenNotifier;
 
+	private AnimalState _state = AnimalState.READY;
+	private float _arrowScaleX = 0.0f;
+	private Vector2 _start = Vector2.Zero;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_visibleOnScreenNotifier.ScreenExited += OnScreenExited;
+		ConnectSignals();
 	}
 
-    private void OnScreenExited()
+	private void InitializeVariables()
+	{
+		_start = Position;
+		_arrowScaleX = _arrow.Scale.X;
+		_arrow.Hide();
+	}
+
+	public void ConnectSignals()
+	{
+		_visibleOnScreenNotifier.ScreenExited += OnScreenExited;
+		SleepingStateChanged += OnSleepingStateChanged;
+		InputEvent += OnInputEvent;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		UpdateDebugLabel();
+	}
+
+	private void UpdateDebugLabel()
+	{
+		_debugLabel.Text = $"St:{_state} Sl:{Sleeping}";
+	}
+
+	private void OnInputEvent(Node viewport, InputEvent @event, long shapeIdx)
+	{
+		GD.Print(@event);
+	}
+
+    private void OnSleepingStateChanged()
+    {
+    }
+
+	private void OnScreenExited()
     {
 		GD.Print("OnScreenExited");
     }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-	{
-	}
 }
